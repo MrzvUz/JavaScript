28561e07ed27a6b2942bf8abb1e0fb8d4306d2c7
Ali Mirzaev <mrzv.uz@gmail.com>
2021-07-15 09:23:31 -0500

    Commenting in JavaScript

--- Ch01/01_04/test.js
+++ Ch01/01_04/test.js
@@ -3,7 +3,8 @@ var monster1 = "Hello", monster2 = "World", monster3 = "Everyone";
 
 // Strings.
 
-// Objects in 
+// Objects in JavaScript.
+var myObject = {}
 var animal = {
     genus: "corvus",
     species: "corvax",
@@ -17,9 +18,30 @@ var animal = {
 animal1 = animal // Creates a reference and doesn't make a copy of object animal. If animal1 item changed animal item will change too.
 animal2 = JSON.parse(JSON.stringify(animal)); // Creates a copy of the object animal.
 
+
+
 // Arrays [Lists]
-var myArray = []
+var myArray = [];
+var myArray = [0, 1, 2, "string0", "string1", true, false];
 var dayOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", 'Friday', "Saturday", "Sunday"];
+
+
+
+
+var longString = "Four score\
+and seven years ago\
+our fathers brought forth\
+on this continent\
+a new nations.";
+
+
+// Commenting in JavaScript.
+// = Line commenting
+/* Block commenting.
+You can write
+comments in
+multiple lines.
+*/
 
 
 
